66b3fcafed77ade288068d43852c86208a09efa8
Kwitsch <kwitsch@gmail.com>
2020-10-16 10:48:54 +0200

    base structure

---
+++ CERelayBoard8Serial/CERB8S_Controller.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CERelayBoard8Serial
+{
+    public class CERB8S_Controller
+    {
+        internal CERB8S_Controller(string port)
+        {
+
+        }
+    }
+}
